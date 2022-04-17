ce350426d57927e3f6d685d4a42f3b36a2198df4
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-17 15:56:26 +0530

    added css file for coins

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import './App.css';
-import Coin from './Coin.js'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-
-
+import './App.css';
+import Coin from './Coin';
 
 function App() {
-
-  const [coins , setCoins] = useState([]);
-const [search,setSearch] = useState('');
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=Inr&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(
-      res => {
-        setCoins(res.data)
-      }).catch(error =>console.log(error));
-    },[]);
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
-
-  const handleChange = e =>{
+  const handleChange = e => {
     setSearch(e.target.value);
-  }
+  };
 
-  const filterCoins = coins.filter( coin =>{
+  const filteredCoins = coins.filter(coin =>
     coin.name.toLowerCase().includes(search.toLowerCase())
-  })
-
+  );
 
   return (
     <div className='coin-app'>
       <div className='coin-search'>
-        <h1 className='coin-text'> Search a currency </h1>
+        <h1 className='coin-text'>Search a currency</h1>
         <form>
-          <input type="text" placeholder='search' className="coin-input" onChange={handleChange}/>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
         </form>
       </div>
-
-      {filterCoins.map(coin =>{
+      {filteredCoins.map(coin => {
         return (
-          <Coin key={coin.id} name={coin.name} image={coin.mage} symbol={coin.symbol} volume={coin.market_cap} price={coin.current_pirice}/>
-        )
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
       })}
-
     </div>
   );
 }
 
 export default App;
+
+  
--- src/Coin.js
+++ src/Coin.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Coin.css';
 
-export default function Coin(name,image,symbol,price,volume) {
-  return (
-    <div className = 'coin-container'>
-        <div className="coin-row">
-            <div className='coin'>
-                <img src={image} alt='crypto'/>
-                <h1>{name}</h1>
-                <p className='coin-symbol'>{symbol}</p>
-            </div>
-            <div className='coin-data'>
-                <p className='coin-price'> ${price}</p>
-                <p className='coin-volume'>${volume.toLocaleString()}</p>
-            </div>
-        </div>
-    </div>
-  )
+export default function Coin({name,image,symbol,price,priceChange}) {
+    return (
+      <div className = 'coin-container'>
+          <div className="coin-row">
+              <div className='coin'>
+                  <img src={image} alt='crypto'/>
+                  <h1>{name}</h1>
+                  <p className='coin-symbol'>{symbol}</p>
+              </div>
+              <div className='coin-data'>
+                  <p className='coin-price'> Rs. {price}</p>
+                  {priceChange < 0 ? (
+                      <p className='coin-present red' >{priceChange.toFixed(2)} %</p>
+                  ) : (<p className='coin-present green' >{priceChange.toFixed(2)} %</p>
+                  )}
+              </div>
+          </div>
+      </div>
+    )
 }
